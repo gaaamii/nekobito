793a4044a2ad8502a3f58a0e48b54dca1333330f
gaaamii <i1shi4ga4mi1@gmail.com>
2017-11-10 05:25:47 +0900

    Just init view with parsed markdown preview

--- src/Main.elm
+++ src/Main.elm
@@ -1,55 +1,52 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
-
+import Html exposing (Html, text, div, h1, img, header, p, textarea)
+import Html.Attributes exposing (src, class)
+import Markdown exposing (..)
 
 ---- MODEL ----
-
-
 type alias Model =
-    {}
+  { body: String }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
-
-
+  ( { body = "# test" }, Cmd.none )
 
 ---- UPDATE ----
 
 
 type Msg
-    = NoOp
-
+  = NoOp
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
-
-
+  ( model, Cmd.none )
 
 ---- VIEW ----
 
-
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+  div [ class "app-wrapper" ]
+    [ header [ class "app-header" ]
+      [ p []
+        [ text "Nekobito - A Markdown Text Editor on Browser" ]
+      ]
+    , div [ class "app-container" ]
+        [ div [ class "app-editor" ]
+          [ textarea [] 
+            [ text model.body ]
+          ]
+        , Markdown.toHtml [ class "app-preview" ] model.body
         ]
-
-
+      ]
 
 ---- PROGRAM ----
-
-
 main : Program Never Model Msg
 main =
-    Html.program
-        { view = view
-        , init = init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+  Html.program
+    { view = view
+    , init = init
+    , update = update
+    , subscriptions = always Sub.none
+    }
