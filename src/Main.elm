866c8722f52d05e14bdb38e3aa38a17d6aabcac0
gaaamii <i1shi4ga4mi1@gmail.com>
2017-11-26 18:26:24 +0900

    add actions

--- src/Main.elm
+++ src/Main.elm
@@ -1,71 +1,98 @@
 port module Main exposing (..)
 
-import Html exposing (Html, text, div, img, header, p, textarea, button)
-import Html.Attributes exposing (src, class, placeholder, value)
-import Html.Events exposing (onInput)
+import Html exposing (Html, button, div, header, img, p, text, textarea)
+import Html.Attributes exposing (class, placeholder, src, value)
+import Html.Events exposing (onClick, onInput)
 import Markdown
 
+
 ---- MODEL ----
+
+
 type alias Model =
-  { body: String }
+    { body : String }
+
 
 emptyModel : Model
 emptyModel =
-  {
-    body = ""
-  }
+    { body = ""
+    }
+
 
 init : Maybe Model -> ( Model, Cmd Msg )
 init savedModel =
-  Maybe.withDefault emptyModel savedModel ! []
+    Maybe.withDefault emptyModel savedModel ! []
+
+
 
 ---- UPDATE ----
+
+
 type Msg
-  = OnInput String
+    = OnInput String
+    | DeleteEntry
+    | ListEntries
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    OnInput newBody -> 
-      ( { model | body = newBody }, Cmd.none )
+    case msg of
+        OnInput newBody ->
+            ( { model | body = newBody }, Cmd.none )
+
+        DeleteEntry ->
+            ( model, Cmd.none )
+
+        ListEntries ->
+            ( model, Cmd.none )
+
+
 
 ---- VIEW ----
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "app-wrapper" ]
-    [ header [ class "app-header" ]
-      [ p []
-        [ text "Nekobito - A Markdown Text Editor on Browser"
-        , button [ class "btn-list" ] [ text "List" ]
-        , button [ class "btn-delete" ] [ text "Delete" ]
+    div [ class "app-wrapper" ]
+        [ header [ class "app-header" ]
+            [ p []
+                [ text "Nekobito - A Markdown Text Editor on Browser"
+                , button [ class "btn-list", onClick ListEntries ] [ text "List" ]
+                , button [ class "btn-delete", onClick DeleteEntry ] [ text "Delete" ]
+                ]
+            ]
+        , div [ class "app-container" ]
+            [ div [ class "app-editor" ]
+                [ textarea [ onInput OnInput, placeholder "# Markdown text here", value model.body ] []
+                ]
+            , Markdown.toHtml [ class "app-preview" ] model.body
+            ]
         ]
-      ]
-    , div [ class "app-container" ]
-        [ div [ class "app-editor" ]
-          [ textarea [ onInput OnInput, placeholder "# Markdown text here" , value model.body ] []
-          ]
-        , Markdown.toHtml [ class "app-preview" ] model.body
-        ]
-    ]
+
 
 port setStorage : Model -> Cmd msg
 
+
 updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
 updateWithStorage msg model =
-  let
-    ( newModel, cmds ) =
-      update msg model
-  in
+    let
+        ( newModel, cmds ) =
+            update msg model
+    in
     ( newModel
     , Cmd.batch [ setStorage newModel, cmds ]
     )
 
+
+
 ---- PROGRAM ----
+
+
 main : Program (Maybe Model) Model Msg
 main =
-  Html.programWithFlags
-    { view = view
-    , init = init
-    , update = updateWithStorage
-    , subscriptions = always Sub.none
-    }
+    Html.programWithFlags
+        { view = view
+        , init = init
+        , update = updateWithStorage
+        , subscriptions = always Sub.none
+        }
