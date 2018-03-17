c5110b8363535d324af65ec851701107485648a0
gaaamii <i1shi4ga4mi1@gmail.com>
2018-03-17 18:41:19 +0900

    update pre style

--- src/Styles.elm
+++ src/Styles.elm
@@ -9,7 +9,7 @@ appContainer isVisible =
     if isVisible then
         style <|
             [ ( "margin-left", "0" )
-            , ( "width", "65%" )
+            , ( "width", "80%" )
             ]
     else
         style <| [ ( "margin-left", "0" ) ]
@@ -20,16 +20,17 @@ appList isVisible =
     let
         baseStyles =
             [ ( "position", "absolute" )
-            , ( "width", "35%" )
+            , ( "width", "20%" )
             , ( "height", "100%" )
             , ( "background", "#222" )
             , ( "top", "0" )
-            , ( "right", "-35%" )
+            , ( "right", "-20%" )
             , ( "color", "#fff" )
+            , ( "font-size", ".8em" )
             , ( "text-align", "center" )
             ]
     in
     if isVisible then
         style <| baseStyles ++ [ ( "right", "0" ) ]
     else
-        style <| baseStyles ++ [ ( "right", "-35%" ) ]
+        style <| baseStyles ++ [ ( "right", "-20%" ) ]
